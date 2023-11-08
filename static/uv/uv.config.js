0be30bf21189ba47057d0c71bb921b6c5692bb1b
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-11-08 19:54:36 +0100

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: '/uv/service/',
-	bare: `${location.protocol}//${location.host}/bare/`,
+	bare: `${location.protocol}//hypertabs-production-5eb8.up.railway.app/bare/`,
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: '/uv/uv.handler.js',
