861d74112da1e2567bda03d04103f823837c4035
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-11-09 18:35:52 +0100

    Update uv.sw.js

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('https://hypertabs-production-5eb8.up.railway.app/uv/uv.bundle.js');
+importScripts('https://hypertabs-production-5eb8.up.railway.app/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
