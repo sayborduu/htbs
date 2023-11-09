7538e42d5781d3240d9b09d746fac95bf1961ebc
SayBored <62549299+sayborduu@users.noreply.github.com>
2023-11-09 18:46:04 +0100

    Update manager.js

--- static/internal/manager.js
+++ static/internal/manager.js
@@ -1,8 +1,8 @@
 const internal_pages = {
     // YOU MUST ADD YOUR INTERNAL PAGE TO THIS FILE IF YOU WANT IT TO WORK!
     // This index is how HT can reference what page to what file
-    newtab: "/internal/newTab/main.html",
-    extensionsmarketplace: "/internal/extensions/marketplace.html",
-    extensions: "/internal/extensions/index.html",
-    games: "/internal/g/index.html"
+    newtab: "https://hypertabs-production-5eb8.up.railway.app/internal/newTab/main.html",
+    extensionsmarketplace: "https://hypertabs-production-5eb8.up.railway.app/internal/extensions/marketplace.html",
+    extensions: "https://hypertabs-production-5eb8.up.railway.app/internal/extensions/index.html",
+    games: "https://hypertabs-production-5eb8.up.railway.app/internal/g/index.html"
 }
